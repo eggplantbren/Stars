2d29d65c5057c28ab6e4d20593264218d1435d4d
Brendon J. Brewer <brendon.brewer@gmail.com>
2024-12-11 15:39:20 +1300

    Renamed file, save new data

--- process_data.py
+++ process_data.py
@@ -6,9 +6,13 @@ data = pd.read_csv("stars_edited.txt", delim_whitespace=True, header=None)
 ra = data.iloc[:,2]
 dec = data.iloc[:,3]
 v = data.iloc[:,6] - np.mean(data.iloc[:,6])
+verr = data.iloc[:,7] - np.mean(data.iloc[:,7])
 
 x = (ra - ra.mean())*np.cos(np.mean(dec))
 y = (dec - dec.mean())
+
+data = np.vstack([x, y, v, verr]).T
+np.savetxt("data.txt", data)
 
 plt.scatter(x[v > 0], y[v > 0], s=10.0*np.abs(v[v > 0]),
             marker="o", color="red", alpha=0.5)
