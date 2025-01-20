b1e672c76ec6ef4e405a8d2b62d008e2a632d934
Brendon J. Brewer <brendon.brewer@gmail.com>
2025-01-21 11:17:02 +1300

    Basic corner plot

--- cpp/showresults.py
+++ cpp/showresults.py
@@ -1,3 +1,20 @@
 import dnest4.classic as dn4
 dn4.postprocess()
 
+import corner
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set up fonts
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.size"] = 14
+plt.rc("text", usetex=True)
+
+posterior_sample = np.loadtxt("posterior_sample.txt")
+corner.corner(posterior_sample,
+    labels=["$\\mu_v$", "$\\sigma_v$"], plot_contours=False,
+        plot_density=False, fontsize=14)
+plt.show()
+
+#plt.savefig("cornerplot.png", dpi=450)
+
